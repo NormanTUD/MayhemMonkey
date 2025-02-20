d64e2cdad79239fc67b3958c111e3ad583bb1b0b
Norman Koch <norman.koch@tu-dresden.de>
2025-02-20 07:30:13 +0100

    fix

--- example2.py
+++ example2.py
@@ -6,9 +6,6 @@ mayhemmonkey.set_function_fail_after_count("print", 3)
 
 mayhemmonkey.install_faulty()
 
-with open("test.txt", "w") as f:
-    f.write("Hello world!")
-
 print("This should be printed.")
 print("This should be printed.")
 print("This should be printed.")
