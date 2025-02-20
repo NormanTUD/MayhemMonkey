881064539aae5734465375f92f58583c98e00a9a
Norman Koch <norman.koch@tu-dresden.de>
2025-02-20 07:32:59 +0100

    fix

--- example2.py
+++ example2.py
@@ -5,7 +5,6 @@ mayhemmonkey = MayhemMonkey()
 mayhemmonkey.set_function_fail_after_count("print", 3)
 
 mayhemmonkey.install_faulty()
-mayhemmonkey.install_faulty()
 
 print("This should be printed.")
 print("This should be printed.")
